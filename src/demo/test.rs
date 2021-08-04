a6b619c43c1cc81ea761fd011b9ab39a83c7ea95
小呆呆666 <xdd666t@gmail.com>
2021-08-04 22:34:27 +0800

    commit

--- src/demo/test.rs
+++ src/demo/test.rs
@@ -1,17 +1,26 @@
 pub fn test() {
-    let string1 = String::from("long string is long");
-    let result;
-    {
-        let string2 = String::from("xyz");
-        result = longest(string1.as_str(), string2.as_str());
-    }
-    println!("The longest string is {}", result);
+    let p = Processor {
+        callback: simple_callback,
+    };
+    p.process_events(); // hello world!
 }
 
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() {
-        x
-    } else {
-        y
+type Callback = fn();
+
+struct Processor {
+    callback: Callback,
+}
+
+impl Processor {
+    fn set_callback(&mut self, c: Callback) {
+        self.callback = c;
     }
+
+    fn process_events(&self) {
+        (self.callback)();
+    }
+}
+
+fn simple_callback() {
+    println!("hello world!");
 }
