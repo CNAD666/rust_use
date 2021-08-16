423f3cf429618be5110531393544404844f53116
小呆呆666 <xdd666t@gmail.com>
2021-08-16 22:28:35 +0800

    Update mod.rs

--- src/leet_code/sword_offer/chain_table/mod.rs
+++ src/leet_code/sword_offer/chain_table/mod.rs
@@ -7,8 +7,12 @@ pub fn main() {
     let default = 1;
 
     match default {
+        // 链表反转打印
         1 => reverse_print::main(),
+        // 链表反转
         2 => reverse_list::main(),
+        // 复杂链表复制,暂不支持rust
+        3 => chain_copy::main(),
         _ => {}
     }
 }
